79fea6cc32a3802349380be6539d9f5e5f1baedc
GouthamGuna <ggowthamsankar@gmail.com>
2024-04-04 05:46:14 +0530

    Update GenerateQrCodeApplication.java

--- src/main/java/in/dev/gmsk/GenerateQrCodeApplication.java
+++ src/main/java/in/dev/gmsk/GenerateQrCodeApplication.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class GenerateQrCodeApplication {
 
     public static void main(String[] args) {
+        System.setProperty("server.servlet.context-path", "/QR_Generate");
         SpringApplication.run( GenerateQrCodeApplication.class, args );
     }
 
